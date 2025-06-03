c76eceb26d542fa4b8ddff8e1f54a485ecea53f7
nagiyu <otonagiyu@gmail.com>
2025-06-03 19:08:36 +0900

    TestControllerにApiController属性を追加

--- Line/Controllers/TestController.cs
+++ Line/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Line.Controllers
 {
+    [ApiController]
     public class TestController : Controller
     {
         private readonly LogService logService;
